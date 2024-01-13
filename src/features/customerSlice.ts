29c80dcd914d5cbc6f63368532d3c9e45e384e8e
behnamhamidi1991 <eslp91@hotmail.com>
2024-01-13 21:05:50 +0330

    customer state

---
+++ src/features/customerSlice.ts
@@ -0,0 +1,25 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface CustomerState {
+  id: string;
+  name: string;
+  food: string[];
+}
+
+interface CustomerState {
+  value: Customer[];
+}
+
+const initialState: CustomerState = {
+  value: [],
+};
+
+export const customerSlice = createSlice({
+  name: "customers",
+  initialState,
+  reducers: {},
+});
+
+export const {} = customerSlice.actions;
+
+export default customerSlice.reducer;
